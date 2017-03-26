eeab28593de22b6b3ae5b0606dd3932fbe80d01e
九条涼果 <s1131234@gmail.com>
2017-03-26 16:39:30 +0800

    添加幂函数的输入方式，现在存在：无法输入负数，可以输入其他符号等BUG

--- No2/main.cpp
+++ No2/main.cpp
@@ -62,7 +62,9 @@ void q_init(Queue<int>& queue) {
         std::cout << "队首元素：" << queue.Peek() << std::endl;
       } catch (std::underflow_error& error) { std::cout << error.what() << std::endl; }
     } else {
-      queue.Enqueue(stoi(input)); // 字符串转整数
+      try {
+        queue.Enqueue(stoi(input)); // 字符串转整数
+      } catch (std::overflow_error& error) { std::cout << error.what() << std::endl; }
     }
   }
   std::cout << queue;
@@ -86,21 +88,13 @@ auto CmpOperator = [](std::string& op1, std::string& op2) -> int {
   if (op1 == op2) { return 0; }
   if (Precedence(op1) - Precedence(op2) > 0) { return 1; } else { return -1; }
 };
-auto Evaluate = [](std::string& op, std::string& a, std::string& b) -> std::string {
-  /*使用字符串，同时储存整数和小数*/
-  if (a.find('.') != std::string::npos || b.find('.') != std::string::npos) { // 有一个小数
-    double val1 = stod(a), val2 = stod(b);
-    if (op == "+")return std::to_string(val1 + val2);
-    if (op == "-")return std::to_string(val1 - val2);
-    if (op == "*")return std::to_string(val1 * val2);
-    if (op == "/")return std::to_string(val1 / val2);
-  } else { // 都是整数
-    int val1 = stoi(a), val2 = stoi(b);
-    if (op == "+")return std::to_string(val1 + val2);
-    if (op == "-")return std::to_string(val1 - val2);
-    if (op == "*")return std::to_string(val1 * val2);
-    if (op == "/")return std::to_string(val1 / val2);
-  }
+auto Evaluate = [](std::string& op, double val1, double val2) -> double {
+  if (op == "+")return val1 + val2;
+  if (op == "-")return val1 - val2;
+  if (op == "*")return val1 * val2;
+  if (op == "/")return val1 / val2;
+  if (op == "^")return std::pow(val1, val2);
+
   throw std::invalid_argument("不支持的操作符");
 };
 }
@@ -112,7 +106,7 @@ void caculator() {
   std::regex arithmetic_regex(R"((\d+(?:\.\d*)?)|([\(\)\+\-\*\/\^]))");
   while (std::getline(std::cin, input)) {
     /*Shunting Yard Algorithm*/
-    Stack<std::string> result_stack; // 结果栈
+    Stack<double> result_stack; // 结果栈
     Stack<std::string> op_stack; // 操作符栈
     try {
       for (auto i = std::sregex_iterator(input.begin(), input.end(), arithmetic_regex); i != std::sregex_iterator(); ++i) {
@@ -133,7 +127,7 @@ void caculator() {
             result_stack.Push(Evaluate(stack_top, result_stack.Pop(), result_stack.Pop()));
           }
         } else { // Number
-          result_stack.Push(op); // 数字直接放入结果栈
+          result_stack.Push(stod(op)); // 数字直接放入结果栈
         }
       }
       /*RPN Calculator*/
@@ -153,6 +147,7 @@ int main() {
 
   std::cout << "|||循环队列的操作|||" << std::endl;
   std::cout << "输入队列大小：";
+  g_Clean_input();
   int queue_size;
   std::cin >> queue_size;
   Queue<int> queue = Queue<int>(queue_size);
